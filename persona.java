ed117061f9c65420343c4e823840b66634d77e25
Diego <Diego@gmail>
2024-01-11 11:27:52 +0100

    MENSAJE MUY MUY MUY PERO QUE MUY MUYYYYYYYYYYYYYYYYYYYYYYYYYYYY IMPORATNTE ENSERIO ES MUY IMPORTANTE ESTILO SI , MUY MUY PERO MUY IMPORTANTE

--- persona.java
+++ persona.java
@@ -1,2 +1,23 @@
-int edad;
-String nombre;
+String gandor = "El atleti gana 4-2 el jueves que viene y empata a 3 el dia 4"
+string alerta = "Acelera un poco mas no me seas abuelo por favor te lo pido"
+string estopa = "Salimos de la cárcel Metemos la primera En el loro deep purple Chirrían las cuatro ruedas Vamos marcando el paso Vamos rompiendo el hielo No hacemos ni puto caso De las señales del cielo Que no tenemos pasta
+Pues dormimos un rato
+Porque el dormir no gasta
+Sale mucho más barato
+Nos comemos la noche
+Cacho a cacho, gramo a gramo
+Enroscamos el coche
+Y a mañana no llegamos
+La vida en un minuto
+No pasa tan deprisa
+Por si acaso disfruto
+Corre que me da la risa
+Acelera un poco más
+Porque me quedo tonto
+Y vamos muy lentos
+Acelera un poco más
+Corre más que el veneno que llevo dentro
+Y acelera un poco más
+Ahoga en tu laguna
+El pedal del freno
+"
